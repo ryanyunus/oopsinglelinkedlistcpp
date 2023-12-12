d6b432923c7b79144a41b8d41715f41321afcc3d
ryanyunus <ryandinus@gmail.com>
2023-12-13 05:52:57 +0700

    fix doublelinkedlist

--- doublelinkedlist.cpp
+++ doublelinkedlist.cpp
@@ -153,6 +153,40 @@ public:
         head->pssword=x[3];
         }
     }
+    void deleteMiddle(int nomer)
+    {  
+      if(head == NULL)
+        {
+            cout << "double linked belum dibuat" << endl;
+        }else{
+            if(nomer == 1)
+            {
+                cout << "bukan posisi tengah" << endl;
+            }else if(nomer>countdouble()){
+                cout << "posisi di luar jangkauan" << endl;
+            }else{
+                Supermarket *cur = new Supermarket();
+                Supermarket *newnode = new Supermarket();
+                Supermarket *afternode = new Supermarket(); 
+                Supermarket *del = new Supermarket(); 
+                
+            
+            cur = head;
+            int hitung =1;
+            while(hitung<nomer-1)
+            {
+                cur = cur->next;
+                hitung++;
+            }
+            // cur->next = del;
+            del = cur->next;
+            afternode = del->next;
+            cur->next = afternode;
+            afternode->prev = cur;
+            delete del;         
+            }
+        }
+    }
     void addMiddle(string x[4],int nomer)
     {  
       if(head == NULL)
@@ -208,9 +242,12 @@ int main()
     ada.ubahakhir(data3);
     ada.ubahawal(dataupdatefirst);
     ada.addMiddle(datatengah,2);
+    ada.printdouble();
+    ada.deleteMiddle(2);
     // ada.removeFirst();
     // ada.removeLast();
     ada.printdouble();
+
 
     
 
