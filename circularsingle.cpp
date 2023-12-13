fd50142369b8a14d657906a2c186d2f30ea35389
ryanyunus <ryandinus@gmail.com>
2023-12-13 07:07:23 +0700

    circular sngle

---
+++ circularsingle.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+using namespace std;
+
+class School{
+public:
+    string nama,nim,email;
+    School *next;
+};
+class Singlelinked{
+public:
+    School *kepala;
+    School *buntut;
+    School *cur;
+    School *del;
+    Singlelinked(){
+        kepala = nullptr;
+        buntut = nullptr;
+        // cur = nullptr;
+    }
+    void createcircular(string y[3])
+    {
+        kepala = new School;
+        kepala->nama = y[0];
+        kepala->nim = y[1];
+        kepala->email = y[2];
+        buntut = kepala;
+        buntut->next = kepala;
+    }
+     void addfirst(string y[3])
+    {
+        School *newnode = new School();
+        newnode->nama = y[0];
+        newnode->nim = y[1];
+        newnode->email = y[2];
+        newnode->next = kepala;
+        buntut->next = newnode;
+        kepala = newnode;
+    }
+    void addlast(string y[3])
+    {
+        School *newnode = new School();
+        newnode->nama = y[0];
+        newnode->nim = y[1];
+        newnode->email = y[2];
+        newnode->next = kepala;
+        buntut->next = newnode;
+        buntut = newnode;
+    }
+    void removefirst()
+    {
+        del = kepala;
+        kepala = kepala->next;
+        buntut->next = kepala;
+        delete del;
+    }
+     void removelast()
+    {
+        del = buntut;
+        cur = kepala;
+        while(cur->next!=buntut)
+        {
+            cur=cur->next;
+        }
+        buntut = cur;
+        buntut->next = kepala;
+        delete del;
+    }
+    void printcircular()
+    {
+        if (kepala == nullptr) {
+            cout << "Daftar kosong" << endl;
+            return;
+        }
+        cout << "Data mahasiswa" << endl;
+        cout << "=====================================" <<endl;
+        cout << "|NPM\t\t|Nama\t\t|Jurusan|" <<endl;
+        cout << "=====================================" <<endl;
+        
+        cur = kepala;
+        while(cur->next != kepala)
+        {
+
+             cout << "|" << cur->nama <<"\t\t|" << cur->nim <<"\t\t|"<< cur->email <<"|" <<endl;
+             cur = cur->next;
+             cout << "=====================================" <<endl;
+        }
+        
+        cout << "|" << cur->nama <<"\t\t|" << cur->nim <<"\t\t|"<< cur->email <<"|" <<endl;
+        cout << "=====================================" <<endl;
+       
+
+    }
+    
+};
+
+int main()
+{
+    Singlelinked objbaru;
+    string data[3] = {"diky","001","gmail"};
+    string data1[3] = {"riky","003","gmail"};
+    string data2[3] = {"ryan","002","gmail"};
+    objbaru.createcircular(data);
+    objbaru.addfirst(data2);
+    objbaru.addlast(data1);
+    // objbaru.removefirst();
+    objbaru.removelast();
+    objbaru.printcircular();
+
+    return 0;
+}
+  
+  
